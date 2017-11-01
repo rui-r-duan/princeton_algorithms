9e5aff4b0f7fd68d25bcc96ffd012bf9e822e36f
Ryan Duan <duanpanda@gmail.com>
2017-11-01 14:59:14 -0400

    add validation in RdTransaction, better compareTo() and toString()

--- 12oop/exercises/RdTransaction.java
+++ 12oop/exercises/RdTransaction.java
@@ -8,16 +8,20 @@ public class RdTransaction implements Comparable<RdTransaction> {
     private final double amount;
     
     public RdTransaction(String who, SmartDate when, double amount) {
+        if (Double.isNaN(amount) || Double.isInfinite(amount))
+            throw new IllegalArgumentException("Amount cannot be NaN or infinite");
         this.who = who;
         this.when = when;     // alias
         this.amount = amount; // alias
     }
     
     public RdTransaction(String transaction) {
-        String[] a = transaction.split(" ");
+        String[] a = transaction.split("\\s+");
         who = a[0];
         when = new SmartDate(a[1]);
         amount = Double.parseDouble(a[2]);
+        if (Double.isNaN(amount) || Double.isInfinite(amount))
+            throw new IllegalArgumentException("Amount cannot be NaN or infinite");
     }
     
     public String who() {
@@ -34,26 +38,33 @@ public class RdTransaction implements Comparable<RdTransaction> {
     
     @Override
     public String toString() {
-        return "[" + who + ", " + when + ", " + amount + "]";
+        // "%-10s" indicates left-alighed
+        return String.format("%-10s %10s %10.2f", who, when, amount);
     }
     
     /**
      * Compares two transactions by amount.
      * 
      * @param  that the other transaction
-     * @return 
+     * @return a negative integer if this is less than that;
+     *         a positive integer if this is greater than that;
+     *         zero if this is equal than that
      */
     @Override
     public int compareTo(RdTransaction that) {
-        return (int)(this.amount - that.amount);
+        // The following "minus" solution does not work correctly for
+        //    (-0.0, 0.0), (!NaN, NaN) ==> should return -1 (less than)
+        //    (0.0, -0.0), (NaN, !NaN) ==> should return +1 (greater than)
+        // return (int)(this.amount - that.amount);
+        return Double.compare(this.amount, that.amount);
     }
 
     public static void main(String[] args) {
         RdTransaction[] a = new RdTransaction[4];
-        a[0] = new RdTransaction("Turing 6/17/1990 644.08");
-        a[1] = new RdTransaction("Tarjan 3/26/2002 4121.85");
-        a[2] = new RdTransaction("Knuth 6/14/1999 288.34");
-        a[3] = new RdTransaction("Dijkstra 8/22/2007 2678.40");
+        a[0] = new RdTransaction("Turing   6/17/1990  644.08");
+        a[1] = new RdTransaction("Tarjan   3/26/2002 4121.85");
+        a[2] = new RdTransaction("Knuth    6/14/1999  288.34");
+        a[3] = new RdTransaction("Dijkstra 8/22/2007 362678.40");
         for (RdTransaction t : a) {
             StdOut.println(t);
         }
