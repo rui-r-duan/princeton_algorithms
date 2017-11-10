rui-r-duan/princeton_algorithms

commit 2bc423617c67756c712cb81eb9eb19714cc8f272

Coursera assignment: fix the 'backwash' minor bug in Percolation

15uf/exercises/Percolation.java
@@ -14,6 +14,9 @@ public class Percolation {
      * create n-by-n grid, with all sites blocked
      */
     public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("invalid n, n must be positive");
+        }
         this.n = n;
         grid = new int[n][n];
 
@@ -99,7 +102,7 @@ public class Percolation {
         if (row == 1) {
             wquf.union(getUFIndex(currentSite), top);
         }
-        if (row == n) {
+        if (row == n && isFull(row, col)) {
             wquf.union(getUFIndex(currentSite), bottom);
         }
         int[] upIndex = new int[] { row-1, col };
@@ -120,11 +123,8 @@ public class Percolation {
     }
     
     private void connect(int[] s, int[] t) {
-//        StdOut.println("connect [" + s[0] + "," + s[1] + "] and ["
-//                           + t[0] + "," + t[1] + "]");
         int si = getUFIndex(s);
         int ti = getUFIndex(t);
-//        StdOut.println("union(" + si + "," + ti + ")");
         wquf.union(si, ti);
     }
     

commit f0b921d5dd8e6156465accd68f62c85ef1b963a4

fix the bug introduced by commit 2bc423617 that cannot percolate

15uf/exercises/Percolation.java
@@ -46,6 +46,14 @@ public class Percolation {
             numberOfOpenSites++;
             connectNeighbors(row, col);
         }
+        // Check each bottom site to see if they are full or not.
+        // If they are full, then connect them to the virtual bottom site.
+        for (int i = 1; i <= n; i++) {
+            if (isOpen(n, i) && isFull(n, i)) {
+                int[] currentSite = new int[] { n, i };
+                wquf.union(getUFIndex(currentSite), bottom);
+            }
+        }
     }
 
     /**

15uf/exercises/PercolationStats.java
@@ -1,6 +1,5 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.Stopwatch;
 
 public class PercolationStats {
     private Percolation p;
@@ -68,13 +67,10 @@ public class PercolationStats {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
-        Stopwatch stopwatch = new Stopwatch();
         PercolationStats stats = new PercolationStats(n, trials);
-        double elapsedTime = stopwatch.elapsedTime();
         StdOut.printf("%-23s = %.16f\n", "mean", stats.mean());
         StdOut.printf("%-23s = %.18f\n", "stddev", stats.stddev());
         StdOut.printf("%-23s = [%.16f, %.16f]\n", "95% confidence interval",
                       stats.confidenceLo(), stats.confidenceHi());
-        StdOut.printf("elapsedTime = %f\n", elapsedTime);
     }
 }