8f8146741f1973389c3d55fd4d31dbee5689c2a5
Ryan Duan <duanpanda@gmail.com>
2017-12-04 23:19:56 -0500

    add comments for StackSortable.java

--- 13stacks/exercises/StackSortable.java
+++ 13stacks/exercises/StackSortable.java
@@ -1,3 +1,55 @@
+/*******************************************************************************
+ * This program answers the question: given a permutation of integers 0 to n-1,
+ * can a stack sort it in ascending order?
+ *
+ * Pattern 231 cannot be sorted by a single stack, according to Donald Knuth.
+ * for a[i], a[j], a[k], where i < j < k,
+ * if a[k] < a[i] < a[j], then it is pattern 231.
+ *
+ * This program can also sort the given permutation. If it is sortable, the
+ * returned result is sorted, if it is not sortable, the returned result is not
+ * sorted.
+ *
+ * Sample execution:
+ * $ java-algs4 StackSortable < stack_permutable_in
+ * (4 3 2 1 0 9 8 7 6 5) can be sorted by a stack? true
+ * Trying to sort it ...:
+ * (0 1 2 3 4 5 6 7 8 9)
+ *
+ * (4 6 8 7 5 3 2 9 0 1) can be sorted by a stack? false
+ * Trying to sort it ...:
+ * (4 6 2 3 5 7 8 0 1 9)
+ *
+ * (2 5 6 7 4 8 9 3 1 0) can be sorted by a stack? false
+ * Trying to sort it ...:
+ * (2 5 6 4 7 8 0 1 3 9)
+ *
+ * (1 2 3 4 5 6 9 8 7 0) can be sorted by a stack? false
+ * Trying to sort it ...:
+ * (1 2 3 4 5 6 0 7 8 9)
+ *
+ * (0 4 6 5 3 8 1 7 2 9) can be sorted by a stack? false
+ * Trying to sort it ...:
+ * (0 4 3 5 6 1 2 7 8 9)
+ *
+ * (1 4 7 9 8 6 5 3 0 2) can be sorted by a stack? false
+ * Trying to sort it ...:
+ * (1 4 7 0 2 3 5 6 8 9)
+ *
+ * (2 1 4 3 6 5 8 7 9 0) can be sorted by a stack? false
+ * Trying to sort it ...:
+ * (1 2 3 4 5 6 7 8 0 9)
+ *
+ * (2 0 1) can be sorted by a stack? true
+ * Trying to sort it ...:
+ * (0 1 2)
+ *
+ * (1 2 0) can be sorted by a stack? false
+ * Trying to sort it ...:
+ * (1 0 2)
+ *
+ * @author Ryan Duan <duanpanda@gmail.com>
+ ******************************************************************************/
 import edu.princeton.cs.algs4.Stack;
 
 public class StackSortable {
@@ -22,7 +74,7 @@ public class StackSortable {
     public static int[] sort(int[] target) {
         if (target.length == 0)
             return null;
-        
+
         int[] result = new int[target.length];
         int j = 0;              // index for result[]
         Stack<Integer> stack = new Stack<Integer>();
