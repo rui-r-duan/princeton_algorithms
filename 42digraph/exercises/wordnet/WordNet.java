c6e671c83e6f42a6272604b038fef2a8bcfc98ea
Ryan Duan <duanpanda@gmail.com>
2018-01-17 23:58:40 -0500

    Coursera assignment WordNet: WordNet data structure OK

--- 42digraph/exercises/wordnet/WordNet.java
+++ 42digraph/exercises/wordnet/WordNet.java
@@ -1,10 +1,14 @@
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdOut;
-// import edu.princeton.cs.algs4.Out;
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.ST;
+import edu.princeton.cs.algs4.SET;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Out;
 
 public class WordNet {
     private final Digraph digraph;
+    private final ST<String, SET<Integer>> map; // <noun, synset_id list>
     private final SAP sap;
 
     /**
@@ -14,6 +18,24 @@ public class WordNet {
         if (synsets == null || hypernyms == null)
             throw new IllegalArgumentException("arguments cannot be null");
 
+        map = new ST<String, SET<Integer>>();
+        int vertexCnt = readSynsets(synsets);
+        StdOut.println("vertexCnt=" + vertexCnt);
+        digraph = new Digraph(vertexCnt);
+        readHypernyms(hypernyms);
+        sap = new SAP(digraph);
+        StdOut.println("G.V()=" + digraph.V());
+        StdOut.println("G.E()=" + digraph.E());
+    }
+
+    // Input synsets from a file, store all the nouns and return the vertex
+    // number (number of synsets).
+    // It should be called only by the constructor.
+    // @param synsets the input file name
+    // @return number of synsets
+    // @pre map != null
+    private int readSynsets(String synsets) {
+        assert map != null;
         StdOut.println(synsets);
         In synsetsIn = new In(synsets);
         int vertexCnt = 0;
@@ -25,14 +47,29 @@ public class WordNet {
             int synsetID = Integer.parseInt(fields[0]);
             String[] words = fields[1].split(" ");
             // String gloss = fields[2];
-            // for (int i = 0; i < words.length; i++) {
-            // }
+            for (int i = 0; i < words.length; i++) {
+                SET<Integer> set = map.get(words[i]);
+                if (set == null) {
+                    set = new SET<Integer>();
+                    set.add(synsetID);
+                }
+                else {
+                    set.add(synsetID);
+                }
+                map.put(words[i], set);
+            }
             vertexCnt++;
         }
         synsetsIn.close();
+        return vertexCnt;
+    }
 
-        digraph = new Digraph(vertexCnt);
-
+    // Input hypernyms from a file, and build up the digraph.
+    // It should be called only by the constructor.
+    // @param hypernyms the input file name
+    // @pre digraph != null
+    private void readHypernyms(String hypernyms) {
+        assert digraph != null;
         StdOut.println(hypernyms);
         In hypernymsIn = new In(hypernyms);
         while (!hypernymsIn.isEmpty()) {
@@ -50,17 +87,13 @@ public class WordNet {
             }
         }
         hypernymsIn.close();
-
-        StdOut.println("V=" + digraph.V());
-        StdOut.println("E=" + digraph.E());
-        sap = new SAP(digraph);
     }
 
     /**
      * returns all WordNet nouns
      */
     public Iterable<String> nouns() {
-        return null;
+        return map;
     }
 
     /**
@@ -69,7 +102,7 @@ public class WordNet {
     public boolean isNoun(String word) {
         if (word == null)
             throw new IllegalArgumentException("arguments cannot be null");
-        return false;
+        return map.contains(word);
     }
 
     /**
@@ -83,8 +116,8 @@ public class WordNet {
     }
 
     /**
-     * a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-     * in a shortest ancestral path (defined below)
+     * a synset (second field of synsets.txt) that is the common ancestor of
+     * nounA and nounB in a shortest ancestral path
      */
     public String sap(String nounA, String nounB) {
         if (nounA == null || nounB == null)
@@ -98,5 +131,16 @@ public class WordNet {
      */
     public static void main(String[] args) {
         WordNet wn = new WordNet(args[0], args[1]);
+        StdOut.println("isNoun(\"Actifed\")? " + wn.isNoun("Actifed"));
+        StdOut.println("isNoun(\"AND_circuit\")? " + wn.isNoun("AND_circuit"));
+        StdOut.println("isNoun(\"antihistamine\")? " + wn.isNoun("antihistamine"));
+        StdOut.println("isNoun(\"nasal_decongestant\")? " + wn.isNoun("nasal_decongestant"));
+        StdOut.println("writing all the words to file \"all_words\"");
+        Iterable<String> words = wn.nouns();
+        Out o = new Out("all_words");
+        for (String s : words) {
+            o.println(s);
+        }
+        o.close();
     }
 }
